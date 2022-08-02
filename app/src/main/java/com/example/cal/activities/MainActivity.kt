e6b1eac63f35f62b6b3f4d7230bad7ec2e4f0950
ha2320 <ngnhan.hoanganh@gmail.com>
2022-08-02 13:24:18 +0700

    all calculations with negative numbers, self-calculation for 1 number and all  rounding have been done, fix output format for decimal numbers

--- app/src/main/java/com/example/cal/activities/MainActivity.kt
+++ app/src/main/java/com/example/cal/activities/MainActivity.kt
@@ -14,13 +14,23 @@ class MainActivity : AppCompatActivity() {
     private var resultStr: String = "" // string value of the calculated result
     private var numb1Str: String = ""
     private var numb2Str: String = ""
-    private var numb1DotPos = -5
-    private var numb2DotPos = -5
-    private var formulaStr: String = "" // represent the math, made up from $numb1Str$currentOperator$numb2Str
+
+    private var formulaStr: String = ""
+    // represent the math
+    // made up from $numb1Str$currentOperator$numb2Str
+
+    // two variables below define whether a number or the math is being edited
     private var editingFirstNumber = true
     private var editingSecondNumber = false
 
-    // invalid position other than -1, because it will be compared with the length of a string
+    /*
+    * all position markers are declared with value of -5
+    * it is an invalid position in a string/array
+    * can not be -1
+    * due to the fact that it will be compared to an_array_length-1
+     */
+    private var numb1DotPos = -5
+    private var numb2DotPos = -5
     private var operatorPosition = -5 // necessary for delete-1-character button
     private var currentOperator: Operator = Operator.UNDEFINED
     private lateinit var digitButtonClickListener: View.OnClickListener
@@ -34,11 +44,16 @@ class MainActivity : AppCompatActivity() {
         // Declare OnClick Listener for Digit Buttons
         digitButtonClickListener = View.OnClickListener {
             digitButton ->
+
+            // if old result is available, clear it and start a new calculation
             if(resultStr.isNotBlank()) {
                 onStartNewCalculation()
                 resultStr = ""
             }
             val numberDigit: String = getNumberStringValueFromDigitButtonID(digitButton!!.id)
+
+            // replace first 0 digits while second character isn't a dot
+            // insert new digit to current number
             if (editingFirstNumber) numb1Str =
                 if (numb1Str != "" && numb1Str.first() == '0' && numb1Str[1] != '.') numberDigit
                 else numb1Str + numberDigit
@@ -51,25 +66,69 @@ class MainActivity : AppCompatActivity() {
         operatorButtonClickListener = View.OnClickListener {
             operatorButton ->
             val newOp: Operator = getOpStringFromOpButton(operatorButton.id)
-            if (numb1Str != ""){
+            val highPriorityOperators = listOf(
+                Operator.MULTIPLY,
+                Operator.DIVIDE,
+                Operator.POWER
+            )
+
+            /*
+            * if minus is pressed
+            * does it define a negative number input or a minus mark?
+            * if currentOp has high priority: number2 is negative
+            * or else, what if currentOp isn't defined?
+            * number2 wouldn't be being edited by then
+            * which makes number1 negative if currently blank
+            * or else it is an ordinary minus math
+            * however, old result is taken as number1 on start of a new calculation
+            */
+            if (
+                newOp == Operator.MINUS
+                && (currentOperator in highPriorityOperators || currentOperator == Operator.UNDEFINED)
+                && resultStr.isBlank()
+            ) {
+                if (editingFirstNumber && numb1Str.isBlank()) numb1Str = "-"
+                else if(editingSecondNumber && numb2Str.isBlank())  numb2Str = "-"
+                else if (numb2Str.isBlank()) onFinishFirstNumber(newOp)
+            }
+            else if (numb1Str.isNotBlank()){
+                // if number1 isn't blank, it would probably have been finished
                 if(resultStr.isNotBlank()) {
                     onStartNewCalculation()
                     numb1Str = resultStr
                     resultStr = ""
                 }
-                editingFirstNumber = false
-                editingSecondNumber = true
-                currentOperator = newOp
-                operatorPosition = numb1Str.length
-                updateFormula()
+
+                /*
+                * finish number1,
+                * specify currentOp,
+                * move on to number2
+                */
+                onFinishFirstNumber(newOp)
             }
+            else if(resultStr.isBlank() && numb1Str.isBlank()){
+                // this is the start of a new calculation, seems negative
+                numb1Str = "-"
+            }
+            updateFormula()
         }
 
         // set onClickListeners
         handleClicks()
     }
+
+    private fun onFinishFirstNumber(newOp: Operator) {
+        editingFirstNumber = false
+        editingSecondNumber = true
+        operatorPosition = numb1Str.length
+        currentOperator = newOp
+        updateFormula()
+    }
+
     private fun handleClicks() {
         // handle clicks for digit buttons
+        /* pressing a number / dot after a math will lead to a new calculation
+        even with the old result stay onscreen */
         binding!!.numb1.setOnClickListener(digitButtonClickListener)
         binding!!.numb2.setOnClickListener(digitButtonClickListener)
         binding!!.numb3.setOnClickListener(digitButtonClickListener)
@@ -81,35 +140,11 @@ class MainActivity : AppCompatActivity() {
         binding!!.numb9.setOnClickListener(digitButtonClickListener)
         binding!!.numb0.setOnClickListener(digitButtonClickListener)
 
-        binding?.dot?.setOnClickListener{
-            if (resultStr.isNotBlank()) {
-                onStartNewCalculation()
-                resultStr = ""
-            }
-            if (editingFirstNumber) {
-                if (numb1Str == "") {
-                    numb1Str = "0."
-                    numb1DotPos = 1
-                }
-                else if ( numb1DotPos == -5){
-                    numb1DotPos = numb1Str.length
-                    numb1Str += "."
-                }
-            }
-            else if(editingSecondNumber) {
-                if (numb2Str == "") {
-                    numb2Str = "0."
-                    numb2DotPos = 1
-                }
-                else if (numb2DotPos == -5) {
-                    numb2DotPos = numb2Str.length
-                    numb2Str += "."
-                }
-            }
-            updateFormula()
-        }
+        binding?.dot?.setOnClickListener{   onDotClickEvent()   }
 
         // handle clicks for operator buttons
+        /* minus mark (" - " ) also define a negative number, therefore should have specific cases handled
+        *  */
         binding!!.opPlus.setOnClickListener(operatorButtonClickListener)
         binding!!.opMinus.setOnClickListener(operatorButtonClickListener)
         binding!!.opMultiply.setOnClickListener(operatorButtonClickListener)
@@ -119,9 +154,32 @@ class MainActivity : AppCompatActivity() {
         // handle clicks for action buttons
         handleDeleteButtonClick()   // split into a separated function for being too long
         binding!!.clearAll.setOnClickListener{clearAll()}
+
+        /*
+         absolutely must press "=" button to show result
+         pressing any other operator button before showing the result will change the math
+         */
         binding!!.showResult.setOnClickListener { showResult() }
     }
 
+    private fun onDotClickEvent() {
+        if (resultStr.isNotBlank()) {
+            // on click =-> start a new math if old result hasn't been cleared
+            onStartNewCalculation()
+            resultStr = ""
+        }
+        // automatically recognize 0. if pressed before an empty number
+        if (editingFirstNumber) {
+            if (numb1Str == "" || numb1Str.startsWith("-")) numb1Str += "0."
+            else if (numb1DotPos == -5) numb1Str += "."
+            numb1DotPos = numb1Str.length
+        } else if (editingSecondNumber) {
+            if (numb2Str == "" || numb2Str.startsWith("-")) numb2Str += "0."
+            else if (numb2DotPos == -5) numb2Str += "."
+            numb2DotPos = numb2Str.length
+        }
+        updateFormula()
+    }
 
     private fun showResult() {
         calculation =
@@ -132,7 +190,7 @@ class MainActivity : AppCompatActivity() {
         } catch (e: Exception){
             binding?.errorTextOutput?.text = e.message
         }
-        binding?.resultOutput?.text = resultStr.ifBlank {"0"}
+        binding?.resultOutput?.text = resultStr.ifBlank {"0.0"}
     }
 
     private fun doMath() = when (currentOperator) {
@@ -141,14 +199,14 @@ class MainActivity : AppCompatActivity() {
         Operator.MULTIPLY -> calculation?.multiply().toString()
         Operator.DIVIDE -> calculation?.divide().toString()
         Operator.POWER -> calculation?.power().toString()
-        else -> numb1Str.ifBlank { "0" }
+        else -> calculation?.formatDecimalBy6Digits(numb1Str.ifBlank { "0" }.toDouble()).toString()
     }
-
 
     private fun clearAll() {
         onStartNewCalculation()
         updateFormula()
-        binding!!.resultOutput.text = "0"
+        resultStr = ""
+        binding!!.resultOutput.text = "0.0"
     }
 
     private fun onStartNewCalculation() {
@@ -163,6 +221,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun handleDeleteButtonClick() {
         binding?.characterDeleter?.setOnClickListener {
+            // set edited object
             when {
                 formulaStr.length-1 == operatorPosition -> {
                     editingFirstNumber = false
@@ -191,6 +250,7 @@ class MainActivity : AppCompatActivity() {
                 editingFirstNumber = true
                 operatorPosition = -5
             }
+            resultStr = ""
             updateFormula()
         }
     }
@@ -223,6 +283,7 @@ class MainActivity : AppCompatActivity() {
     private fun updateFormula() {
         formulaStr =
             if(currentOperator == Operator.UNDEFINED) numb1Str
+            else if (numb2Str.startsWith("-")) "$numb1Str${currentOperator.getVal()}($numb2Str)"
             else "$numb1Str${currentOperator.getVal()}$numb2Str"
         binding!!.formulaOutput.text = formulaStr.ifBlank { "0.0" }
     }
--- app/src/main/java/com/example/cal/models/Calculation.kt
+++ app/src/main/java/com/example/cal/models/Calculation.kt
@@ -4,4 +4,6 @@ import com.example.cal.interfaces.Calculating
 
 class Calculation(override var number1: Number=0, override var number2: Number?): Calculating{
     constructor(number1: Number) : this(number1, null)
+    // secondary constructor, called if number2 is unavailable
+
 }
